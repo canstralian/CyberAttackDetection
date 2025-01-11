40539698df2b144807f057441d63d935423c4183
Esteban Cara de Sexo <8595080+canstralian@users.noreply.github.com>
2025-01-11 20:52:37 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CyberAttackDetection',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'transformers==4.33.3',
+        'torch==2.1.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            # Add your console scripts here
+        ],
+    },
+)
